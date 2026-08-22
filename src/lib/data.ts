fc1845a7a45db9874328a90f5309e2d4483ac3ef
Ibrahim Bello <ibrahimopeyemibello@gmail.com>
2026-08-22 23:00:21 +0100

    update

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,5 +1,11 @@
 import type { NavLink } from "@/lib/types";
 
+import AccessControl from "@assets/images/access-control.png";
+import CCTV from "@assets/images/cctv-surveillance.png";
+import ComputerSolutions from "@assets/images/computer-solutions.png";
+import Intercom from "@assets/images/intercom.png";
+import Networking from "@assets/images/networking.png";
+
 export const navLinks: NavLink[] = [
   { label: "home", href: "/" },
   { label: "about", href: "/about" },
@@ -7,10 +13,10 @@ export const navLinks: NavLink[] = [
   { label: "contact", href: "/contact" },
 ];
 
-export const serviceLinks = [
-  "computer solutions",
-  "cctv & surveillance",
-  "networking",
-  "access control",
-  "intercom systems",
+export const services = [
+  { label: "computer solutions", image: ComputerSolutions },
+  { label: "cctv & surveillance", image: CCTV },
+  { label: "networking", image: Networking },
+  { label: "access control", image: AccessControl },
+  { label: "intercom systems", image: Intercom },
 ];
